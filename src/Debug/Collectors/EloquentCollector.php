ef0796bc67ff90c468760264b2f3603146b96d8e
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-04-27 23:23:45 +0800

    refactor: Enhance timeline data calculation by refining query duration handling in EloquentCollector

--- src/Debug/Collectors/EloquentCollector.php
+++ src/Debug/Collectors/EloquentCollector.php
@@ -92,29 +92,44 @@ class EloquentCollector extends BaseCollector
     {
         $data = [];
         $queries = $this->getQueryLog();
-
+        
+        // Get request start time from CI4's performance metrics if possible
+        $startTime = 0;
+        $lastEndTime = $startTime;
+        
         foreach ($queries as $index => $query) {
-            $duration = 0;
+            // Get raw time value first
+            $rawTime = 0;
             if (isset($query['time'])) {
-                $duration = $this->normalizeTime($query['time']);
+                $rawTime = (float) $query['time'];
             } elseif (isset($query['duration'])) {
-                $duration = $this->normalizeTime($query['duration']);
+                $rawTime = (float) $query['duration'];
             } elseif (isset($query['elapsed'])) {
-                $duration = $this->normalizeTime($query['elapsed']);
+                $rawTime = (float) $query['elapsed'];
             }
-
-            $queryType = preg_match('/^(SELECT|INSERT|UPDATE|DELETE|SHOW|ALTER|CREATE|DROP)/i', $query['query'], $matches)
-                ? strtoupper($matches[1])
+            
+            // Convert to milliseconds if needed
+            if ($rawTime > 0 && $rawTime < 0.1) {
+                $rawTime *= 1000;
+            }
+            
+            $queryType = preg_match('/^(SELECT|INSERT|UPDATE|DELETE|SHOW|ALTER|CREATE|DROP)/i', $query['query'], $matches) 
+                ? strtoupper($matches[1]) 
                 : 'QUERY';
-
+            
+            // Calculate a reasonable start time 
+            // This is an approximation since we don't know exact query start times
+            $start = $lastEndTime - $startTime;
+            $lastEndTime += $rawTime / 1000; // Convert ms back to seconds for next calculation
+            
             $data[] = [
                 'name'      => "#{$index} {$queryType}",
                 'component' => 'Eloquent',
-                'start'     => 0,
-                'duration'  => $duration,
+                'start'     => $start * 1000, // Convert to milliseconds 
+                'duration'  => $rawTime,
             ];
         }
-
+    
         return $data;
     }
 
