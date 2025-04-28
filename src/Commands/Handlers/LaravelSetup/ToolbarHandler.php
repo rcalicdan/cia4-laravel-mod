1cd9db3d0a8617f5b64c5444f50fc9b21ec608cc
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-04-28 19:04:13 +0800

    fix: Correct collector code insertion in addCollectorToContent method

--- src/Commands/Handlers/LaravelSetup/ToolbarHandler.php
+++ src/Commands/Handlers/LaravelSetup/ToolbarHandler.php
@@ -54,7 +54,7 @@ class ToolbarHandler extends SetupHandler
     {
         $pattern = '/(public\s+array\s+\$collectors\s*=\s*\[)([^\]]*?)(\];)/s';
         $collectorCode = <<<'EOD'
-$1$2
+$1
         \Rcalicdan\Ci4Larabridge\Debug\Collectors\EloquentCollector::class,
 $3
 EOD;
