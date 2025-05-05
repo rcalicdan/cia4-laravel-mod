0a1968bf04188a37eb817731f6d871dae75cbd3f
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-05-05 15:54:49 +0800

    feat: Add terminating callbacks to Application for improved lifecycle management

--- src/Blade/Application.php
+++ src/Blade/Application.php
@@ -2,22 +2,39 @@
 
 namespace Rcalicdan\Ci4Larabridge\Blade;
 
+use Closure;
 use Illuminate\Container\Container;
 use Illuminate\View\Factory;
 use Illuminate\Contracts\View\Factory as ViewFactoryContract;
 
 class Application extends Container
 {
+    protected array $terminatingCallbacks = [];
+
+    public function terminating(Closure $callback)
+    {
+        $this->terminatingCallbacks[] = $callback;
+
+        return $this;
+    }
+
+    public function terminate()
+    {
+        foreach ($this->terminatingCallbacks as $terminatingCallback) {
+            $terminatingCallback();
+        }
+    }
+
     public function getNamespace()
     {
         return '';
     }
-    
+
     public function runningInConsole()
     {
         return false;
     }
-    
+
     public function basePath($path = '')
     {
         return $path;
