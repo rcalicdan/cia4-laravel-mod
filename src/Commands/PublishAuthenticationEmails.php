c9dd1278c5780d78b9cbdae71e85b27208e55aa1
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-06-02 00:47:14 +0800

    feat: Implement command to publish authentication email templates

---
+++ src/Commands/PublishAuthenticationEmails.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Rcalicdan\Ci4Larabridge\Commands;
+
+use CodeIgniter\CLI\BaseCommand;
+use CodeIgniter\CLI\CLI;
+use Rcalicdan\Ci4Larabridge\Traits\FilePublisherTrait;
+
+class PublishAuthenticationEmails extends BaseCommand
+{
+    use FilePublisherTrait;
+
+    /**
+     * @var string The group the command is lumped under
+     */
+    protected $group = 'Email';
+
+    /**
+     * @var string The Command's name
+     */
+    protected $name = 'publish:authentication-emails';
+
+    /**
+     * @var string The Command's description
+     */
+    protected $description = 'Publishes authentication emails to the application directory';
+
+    /**
+     * @var string The Command's usage
+     */
+    protected $usage = 'publish:authentication-emails';
+
+    /**
+     * @var array The Command's arguments
+     */
+    protected $arguments = [];
+
+    /**
+     * @var array The Command's options
+     */
+    protected $options = [];
+
+     /**
+     * Executes the command to publish email templates
+     *
+     * @param array $params Command parameters
+     * @return void
+     */
+    public function run(array $params): void
+    {
+        $sourcePath = $this->getSourcePath();
+        $destinationPath = $this->getDestinationPath();
+
+        if ($this->publishFiles($sourcePath, $destinationPath)) {
+            CLI::write('Authentication Email templates published successfully!', 'green');
+        }
+    }
+
+    /**
+     * Gets the source path for email templates
+     *
+     * @return string
+     */
+    private function getSourcePath(): string
+    {
+        return __DIR__ . '/../Views/emails';
+    }
+
+    /**
+     * Gets the destination path for email templates
+     *
+     * @return string
+     */
+    private function getDestinationPath(): string
+    {
+        return APPPATH . 'Views/emails';
+    }
+}
