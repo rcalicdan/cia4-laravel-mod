bf1111a9697ff4c4b0de4503f2cc28599a91bc71
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2025-06-18 23:58:10 +0800

    feat: Implement authorization gate facade with ability definition and policy association methods

---
+++ src/Facades/Gate.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Rcalicdan\Ci4Larabridge\Facades;
+
+use Rcalicdan\Ci4Larabridge\Authentication\Gate as AuthenticationGate;
+
+/**
+ * Facade for authorization gate functionality
+ * 
+ * Provides static access to authorization capabilities including defining abilities,
+ * associating policies, and checking permissions.
+ */
+class Gate
+{
+    /**
+     * Define a new ability
+     * 
+     * @param string $ability The ability identifier
+     * @param callable $callback Authorization callback that returns boolean
+     * @return void
+     */
+    public static function define($ability, $callback): AuthenticationGate
+    {
+        return AuthenticationGate::getInstance()->define($ability, $callback);
+    }
+
+    /**
+     * Associate a policy with a model class
+     * 
+     * @param string $model Model class name
+     * @param string $policy Policy class name
+     * @return void
+     */
+    public static function policy($model, $policy): AuthenticationGate
+    {
+        return AuthenticationGate::getInstance()->policy($model, $policy);
+    }
+
+    /**
+     * Determine if ability is allowed for current user
+     * 
+     * @param string $ability Ability identifier
+     * @param array $arguments Additional parameters
+     * @return bool
+     */
+    public static function allows($ability, $arguments = []): bool
+    {
+        return AuthenticationGate::getInstance()->allows($ability, $arguments);
+    }
+
+    /**
+     * Determine if ability is denied for current user
+     * 
+     * @param string $ability Ability identifier
+     * @param array $arguments Additional parameters
+     * @return bool
+     */
+    public static function denies($ability, $arguments = []): bool
+    {
+        return AuthenticationGate::getInstance()->denies($ability, $arguments);
+    }
+
+    /**
+     * Check if ability is granted
+     * 
+     * @param string $ability Ability identifier
+     * @param array $arguments Additional parameters
+     * @return bool
+     */
+    public static function check($ability, $arguments = []): bool
+    {
+        return AuthenticationGate::getInstance()->check($ability, $arguments);
+    }
+
+    /**
+     * Get registered policy for a class
+     * 
+     * @param object|string $class Class instance or name
+     * @return mixed Associated policy instance or null
+     */
+    public static function getPolicyFor($class)
+    {
+        return AuthenticationGate::getInstance()->getPolicyFor($class);
+    }
+}
